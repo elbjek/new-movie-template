0eeedafe4eca8c1b09adb83b5bc6a15bf3148950
elbjek <lenabjekovic@hotmaill.com>
2018-06-10 15:50:16 +0200

    final push

--- src/components/movie.vue
+++ src/components/movie.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <h1>Search movies here</h1>
+    <h1>Search here </h1>
     <div class="search">
-      <input type="text" placeholder="Search movie" v-model="item" @keyup="search">
+      <input type="text" placeholder="Search here" v-model="item" @keyup="search">
+      <select name="" v-model="searchType" @click="search" id="select" > 
+        <option value="movie">Movies</option>
+        <option value="series">TV Series</option>
+      </select>
     </div>
 
     <div class="container">
-      <div v-for="movie in info" :key="movie.id" class="single-template">
- 
+      <div v-for="movie in info" :key="movie.imdbID" v-if="selectedType" class="single-template">
         <img :src="movie.Poster" alt="">
         <h1>{{movie.Title}}</h1>
         <p>{{movie.Year}}</p>
@@ -28,19 +31,30 @@ export default {
     return{
       info:[],
       item:"",
-      apiUrl: 'http://www.omdbapi.com/?s='+encodeURIComponent(this.item)+'&apikey=e8ed16e8'
+      searchType:''
     }
   },
 
 methods:{
-
   search() {
       this.axios
-      .get(apiUrl)
+      .get('http://www.omdbapi.com/?s='+encodeURIComponent(this.item)+'&apikey=e8ed16e8' + '&type=' + this.searchType)
       .then(response =>(this.info = response.data.Search))
-  }
+  },
 
+
+},
+computed:{
+      selectedType(){
+    if(this.searchType == ""){
+      return this.movie.Type == this.searchType;
+    } else{
+      return true;
+    }
+  }
 }
+
+
 
 
 }
@@ -56,6 +70,12 @@ methods:{
     padding:10px 100px 10px 10px;
     font-weight: bolder;
     outline: none;
+  }
+  #select{
+    height:30px;
+    border-radius: 40px;
+    width:80px;
+    border:0.5px solid rgba($color: #2c3e50, $alpha: 0.6);
   }
 
 }
